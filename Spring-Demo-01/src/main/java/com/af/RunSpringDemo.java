17a3ca98302f9946ca1212d0d19d6c5b0c1fe43d
huiquan.liao <huiquan.liao@ctgtmo.com>
2024-09-28 20:22:43 +0800

    引入依赖，但是没起到什么用的

--- Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
+++ Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
@@ -1,6 +1,7 @@
 package com.af;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.*;
 
 import java.io.IOException;
@@ -27,7 +28,6 @@ public class RunSpringDemo {
         System.out.println("怕了怕了");
         System.out.println("解析@Scope注解，看看是否有设置代理,类为,什么鬼东西");
         System.out.println("你大爷和搜到合法搜地金佛啊受打击无参构造方法");
-
     }
 
 }
