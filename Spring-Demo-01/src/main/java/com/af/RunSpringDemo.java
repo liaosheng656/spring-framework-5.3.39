e1f95fd6a939735ed093d854b9257c21ad607752
huiquan.liao <huiquan.liao@ctgtmo.com>
2024-09-28 19:44:03 +0800

    中文乱码已解决，找到下面两个文件：idea.exe.vmoptions、idea64.exe.vmoptions，在最后一行添加-Dconsole.encoding=UTF-8 -Dfile.encoding=UTF-8

--- Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
+++ Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
@@ -14,15 +14,20 @@ import java.io.IOException;
 @ComponentScan("com.af")
 public class RunSpringDemo {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-		//������������
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RunSpringDemo.class);
+        //������������
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RunSpringDemo.class);
 //		context.getBean("");
-		//����bean
+        //����bean
 //		RunSpringServiceDemo bean = context.getBeanFactory().createBean(RunSpringServiceDemo.class);
 //		AFAnnotationConfigApplicationContext context = new AFAnnotationConfigApplicationContext(RunSpringDemo.class);
-		System.out.println("finish-333333");
-	}
+        System.out.println("finish-333333");
+        System.out.println("file.encoding: {}"+System.getProperty("file.encoding"));
+        System.out.println("怕了怕了");
+        System.out.println("解析@Scope注解，看看是否有设置代理,类为,什么鬼东西");
+        System.out.println("你大爷和搜到合法搜地金佛啊受打击无参构造方法");
+
+    }
 
 }
---
+++ Spring-Demo-01/src/main/java/com/af/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.af.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+    public OrderService(){
+        System.out.println("调用无参构造方法");
+    }
+}
