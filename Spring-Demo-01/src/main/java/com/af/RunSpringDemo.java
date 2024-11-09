1c607e5f7bf297b666f265e2fe426b672cb1b8db
huiquan.liao <huiquan.liao@ctgtmo.com>
2024-11-09 23:59:04 +0800

    【总结】beanDefinition的扫描/bean生命周期/AOP/事务使用-1

--- Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
+++ Spring-Demo-01/src/main/java/com/af/RunSpringDemo.java
@@ -8,7 +8,9 @@ import com.af.service.HelloService;
 import com.af.service.PerSonService;
 import com.af.service.StudentService;
 import com.alibaba.fastjson2.JSONObject;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.*;
 import org.springframework.context.event.ApplicationContextEvent;
@@ -45,13 +47,29 @@ public class RunSpringDemo {
 	}
     public static void main(String[] args) {
 
-		//扫描逻辑（关键）ConfigurationClassUtils.isConfigurationCandidate
-		//判断扫描的文件是否可以成为bean定义ClassPathScanningCandidateComponentProvider.isCandidateComponent
+        /**
+         * 扫描逻辑
+         * {@link ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry(BeanDefinitionRegistry)}
+         * 扫描逻辑（关键）{@link ConfigurationClassUtils#isConfigurationCandidate}
+         * 判断扫描的文件是否可以成为bean定义
+         * {@link ClassPathScanningCandidateComponentProvider#isCandidateComponent(AnnotatedBeanDefinition beanDefinition)}
+         */
+		//
         //@Component、ComponentScan、Import、ImportResource、@Configuration 有这些注解的都可以为配置类
         //核心创建bean、循环依赖
         /**
+         * 创建bean的核心方法
          * {@link org.springframework.beans.factory.support.AbstractBeanFactory#doGetBean}
          * {@link  org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#createBean(java.lang.String, org.springframework.beans.factory.support.RootBeanDefinition, java.lang.Object[])}
+         */
+        /**
+         * bean生命周期
+         * 1、getBean()-->
+         * 2、FactoryBean不会实例化-->填充属性(.getObject())-->FactoryBean初始化后-->
+         * 3、普通bean-->bean实例化-->
+         * 4、判断是否依赖其他类/bean-->先创建依赖的bean-->可能存在循环依赖-->
+         * 5、Bean初始化前-->initializeBean初始化回调-->初始化方法-->Bean初始化后-->注册销毁方法-->加入单例池中-->
+         * 6、销毁前调用销毁方法-->bean销毁-->容器销毁
          */
 		//无配置文件启动
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RunSpringDemo.class);
